b4c0adfe914a33e4eac10c7bc6062172c2d1596f
Nikolay <Nikolayd2002@gmail.com>
2024-03-12 13:49:05 +0100

    cube

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/GHreps/cube_on_cpp
+# Build directory: D:/GHreps/cube_on_cpp/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
